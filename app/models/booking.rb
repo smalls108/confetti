264aaf0ea39877e12b2ae66dfa21c715820b4dc3
Leah Small <small.leah@gmail.com>
2020-11-30 17:27:15 +0800

    created review model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :costume
   belongs_to :user
   validates :start_date, :end_date, presence: true
+  has_one :review
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_090219) do
+ActiveRecord::Schema.define(version: 2020_11_30_091551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 2020_11_30_090219) do
     t.index ["user_id"], name: "index_costumes_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "rating"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +67,5 @@ ActiveRecord::Schema.define(version: 2020_11_30_090219) do
   add_foreign_key "bookings", "costumes"
   add_foreign_key "bookings", "users"
   add_foreign_key "costumes", "users"
+  add_foreign_key "reviews", "bookings"
 end
