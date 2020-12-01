bd211584b5e92affcd77eed62e2b578f53d298d7
Leah Small <small.leah@gmail.com>
2020-12-01 10:36:22 +0800

    added show action and created show view

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,10 +1,19 @@
 class CostumesController < ApplicationController
+
+ before_action :set_costume, only: [:show]
+
   def index
     @costumes = Costume.all
   end
 
+  def show
+    @booking = Booking.new
+    @reviews = @costume.reviews
+  end
+
   private
+
   def set_costume
-    @costume = Costume.find(params[:costume_id])
+    @costume = Costume.find(params[:id])
   end
 end
