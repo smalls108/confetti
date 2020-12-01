a7fa8b76b84fb2cfa7b89d246f5ee7a4fb22c0f6
Leah Small <small.leah@gmail.com>
2020-12-01 17:29:55 +0800

    added pundit for costumes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def dashboard
-    @renter_pending_bookings = current_user.bookings.where(status: "pending")
+    @pending_bookings = current_user.bookings.where(status: "pending")
     @owner_bookings = current_user.costumes.map(&:bookings).flatten
   end
 end
