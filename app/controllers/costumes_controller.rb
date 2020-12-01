659dffa9ad7cb50ac1bdcc0424b722052da8fbe8
Leah Small <small.leah@gmail.com>
2020-12-01 11:14:31 +0800

    added costume controller actions

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -11,6 +11,21 @@ class CostumesController < ApplicationController
     @reviews = @costume.reviews
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
 
   def set_costume
