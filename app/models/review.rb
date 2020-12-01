6af247aa93070c97650874da7e155b281cb97b67
Zulfia Kadarberdieva <zulfiakad@gmail.com>
2020-12-01 17:30:24 +0800

    revies controller and images everywhere

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :booking
-  validates :title, :review, :rating, presence: true
+  validates :title, :rating, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
