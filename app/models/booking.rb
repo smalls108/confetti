c5b3723457605c569105f1b0533da1858f43e0aa
Leah Small <small.leah@gmail.com>
2020-12-01 12:59:47 +0800

    in progress

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,11 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :start_date, :end_date, presence: true
   has_one :review
+
+  before_create :set_status
+
+  def set_status
+    self.status = "pending"
+  end
+
 end
