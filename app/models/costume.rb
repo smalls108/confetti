2c60d1d6fc100108358263ca50a9bc49c1240066
Zulfia Kadarberdieva <zulfiakad@gmail.com>
2020-12-03 16:11:14 +0800

    Tag search done

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,4 +1,5 @@
 class Costume < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   validates :name, :description, :price, :size, presence: true
   validates :price, :price, numericality: { only_integer: true }
@@ -7,9 +8,17 @@ class Costume < ApplicationRecord
   has_many_attached :photos
   has_many :costume_tags
   has_many :tags, through: :costume_tags
-
   geocoded_by :city
   after_validation :geocode, if: :will_save_change_to_city?
+
+  pg_search_scope :tag_search,
+                  # against: [ :title, :synopsis ],
+                  associated_against: {
+                    tags: [:name]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 
   def unavailable_dates
     bookings.where(status: "accepted").pluck(:start_date, :end_date).map do |range|
